ef55e637f66a91bbe654df9ae4d2e8e6029180b9
estellehe <estellehe729@gmail.com>
2017-07-20 22:41:57 -0400

    add from local

---
+++ wiki/wiki_noupdate.py
@@ -0,0 +1,35 @@
+import logging, gensim, bz2file, csv
+from gensim.models.ldamodel import LdaModel
+from numpy import ones
+
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+
+dictionary = gensim.corpora.Dictionary.load_from_text(bz2file.BZ2File('wiki_wordids.txt.bz2'))
+corpus = gensim.corpora.MmCorpus('wiki_tfidf.mm')
+
+with open ('mcdi_word.csv', 'rb')as f:
+    reader = csv.reader(f)
+    wlist = []
+    for row in reader:
+        wlist.append(row)
+
+idlist = []
+
+for row in wlist:
+    idrow = []
+    for key in dictionary.iteritems():
+        if key[1].encode('utf-8') in row:
+            idrow.append(key[0])
+    idlist.append(idrow)
+
+a = 0.05
+ntopic = 75
+eta_arr = ones((ntopic, len(dictionary))) * 0.5
+for x in range(0, len(idlist)):
+    for id in idlist[x]:
+        eta_arr[x, id] *= 100000
+
+
+lda = LdaModel(corpus = corpus, id2word = dictionary, num_topics = ntopic)
+topiclist = lda.print_topics(num_topics = 75, num_words = 50)
+lda.save('wiki_file_75.model')
